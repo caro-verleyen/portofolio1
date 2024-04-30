100175d7372267df20e891d70dc229919719d54c
karaulyna <karaulyna0409@gmail.com>
2024-04-30 14:19:03 +0200

    version1

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>hello</h1>
+</template>
+
+<style >
+
+@font-face {
+    font-family:Jersey25-Regular ;
+    src: url(./assets/Jersey25-Regular.ttf);
+    
+ }   
+body{
+   font-family:"Jersey25-Regular",Verdana, Geneva, Tahoma, sans-serif ;
+   color: rgba(241, 223, 221, 0.925);
+   background: url(./assets/fond.png) ;
+   height: 100vh;
+   
+   
+}
+
+</style>
